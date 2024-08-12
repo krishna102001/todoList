5aa914c14ca48a49db5b92ab9e0ac3df81d61e9e
Krishna Kant Maurya <123314733+krishna102001@users.noreply.github.com>
2024-08-12 16:50:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 mongoose.set("strictQuery", false);
 
 //connect mongodb
-await mongoose.connect("mongodb+srv://admin:admin@todo-db.qmry2i0.mongodb.net/?retryWrites=true&w=majority&appName=todo-db");
+await mongoose.connect("mongodb+srv://admin:password@todo-db.qmry2i0.mongodb.net/?retryWrites=true&w=majority&appName=todo-db");
 
 
 // defining the schema
